bc5b3c148cae43125116ad539b3e47e054d42c41
HybrisofChaos <thomasoosterbeek@hotmail.com>
2021-04-24 15:25:40 +0200

    Project setup

---
+++ project/scripts/player/Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+    [Export]
+    public int speed = 200;
+
+    private Vector2 velocity = new Vector2();
+
+    public void GetInput(){
+        velocity = new Vector2();
+
+        if(Input.IsActionPressed("left")){
+            velocity.x -= 1;
+        }
+        if(Input.IsActionPressed("right")){
+            velocity.x += 1;
+        }
+        if(Input.IsActionPressed("up")){
+            velocity.y -= 1;
+        }
+        if(Input.IsActionPressed("down")){
+            velocity.y +=1;
+        }
+
+        velocity = velocity.Normalized() * speed;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        velocity = MoveAndSlide(velocity);
+    }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+}
