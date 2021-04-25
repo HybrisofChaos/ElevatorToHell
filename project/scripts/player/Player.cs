4e8935771140b96ff7a65ed79fc89537b4022109
HybrisofChaos <thomasoosterbeek@hotmail.com>
2021-04-25 18:35:16 +0200

    Added demonwalker and pistol

--- project/scripts/player/Player.cs
+++ project/scripts/player/Player.cs
@@ -10,32 +10,46 @@ public class Player : KinematicBody2D, IDamageable
     [Export]
     public int speed = 200;
 
+    [Export]
+    public float shootCooldown = 1.5f;
+
+    [Export]
+    public PackedScene bullet = GD.Load<PackedScene>("res://scenes/projectile/Bullet.tscn");
+
     [Signal]
     public delegate void OnPlayerDeath(Node killer);
 
     private Vector2 velocity = new Vector2();
 
     private AnimationPlayer animationPlayer;
+    private Position2D bulletSpawn;
+    private bool canShoot = true;
 
-    public override void _Ready(){
+    public override void _Ready()
+    {
         this.currentHealth = maxHealth;
 
-        animationPlayer = (AnimationPlayer) ((Sprite)FindNode("PlayerSprite")).FindNode("AnimationPlayer");
+        animationPlayer = (AnimationPlayer)((Sprite)FindNode("PlayerSprite")).FindNode("AnimationPlayer");
     }
 
-    public void GetInput(){
+    public void GetInput()
+    {
         velocity = new Vector2();
 
-        if(Input.IsActionPressed("left")){
+        if (Input.IsActionPressed("left"))
+        {
             velocity.x -= 1;
         }
-        if(Input.IsActionPressed("right")){
+        if (Input.IsActionPressed("right"))
+        {
             velocity.x += 1;
         }
-        if(Input.IsActionPressed("up")){
+        if (Input.IsActionPressed("up"))
+        {
             velocity.y -= 1;
         }
-        if(Input.IsActionPressed("down")){
+        if (Input.IsActionPressed("down"))
+        {
             velocity.y += 1;
         }
 
@@ -48,25 +62,32 @@ public class Player : KinematicBody2D, IDamageable
         LookAt(GetGlobalMousePosition());
         velocity = MoveAndSlide(velocity, Vector2.Up);
 
-        if(animationPlayer != null){
-            if(velocity != Vector2.Zero){
+        if (animationPlayer != null)
+        {
+            if (velocity != Vector2.Zero)
+            {
                 animationPlayer.Play("walk");
-            }else{
+            }
+            else
+            {
                 animationPlayer.Stop();
             }
         }
     }
 
-    public void ApplyDamage(Node source, int damage){
+    public void ApplyDamage(Node source, int damage)
+    {
         currentHealth -= damage;
         GD.Print("Yikes, got hit by " + source.Name);
-        if(currentHealth <= 0){
+        if (currentHealth <= 0)
+        {
             Die(source);
         }
 
     }
 
-    private void Die(Node source){
+    private void Die(Node source)
+    {
         GD.Print("Died by the hands of " + source.Name);
         EmitSignal(nameof(OnPlayerDeath), source);
 
